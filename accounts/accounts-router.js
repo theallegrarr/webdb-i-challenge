0fbad57e13f0f0e1bb2437bca4111b0edd9b2d46
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-04 13:07:36 +0100

    update account feature added

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -18,9 +18,19 @@ router.get('/:id', async (req, res) => {
   try {
       const result = await db('accounts').where({ id: req.params.id });
 
+      res.status(200).json({message: `account ${result[0]} updated successfully`});
+  } catch(err) {
+      res.status(404).json({ message: `error fetching account by ID`});
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+      const result = await db('accounts').where({ id: req.params.id }).update(req.body);
+
       res.status(200).json(result);
   } catch(err) {
-      res.status(500).json({ message: `error fetching accounts`});
+      res.status(400).json({ message: `error updating accounts`});
   }
 })
 
@@ -30,7 +40,7 @@ router.post('/', async (req, res) => {
 
       res.status(200).json(result);
   } catch(err) {
-      res.status(500).json({ message: `error fetching accounts`});
+      res.status(400).json({ message: `error creating account`});
   }
 })
 
