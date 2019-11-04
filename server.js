a635f6e73d79dc2bbfeeabd33e1f313ea542683b
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-04 12:40:51 +0100

    get accounts query added

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', async (req, res) => {
+    try {
+        const accounts = await db('accounts');
+        res.status(200).json(accounts);
+    } catch(err) {
+        res.status(500).json({ message: `error fetching accounts`});
+    }
+})
+
 
 
 module.exports = server;
